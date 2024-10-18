d6939eae759c6ee2df4ae5f3e0b1b649961d0684
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-10-18 17:01:45 +0700

    tao vat the tren counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
         Debug.Log("interact");
+        Instantiate(tomatoPrefab, counterTopPoint);
+
     }
 }
