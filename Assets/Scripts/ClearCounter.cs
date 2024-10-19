7a1d1d2d3769984d6c0cf7829eb73dea729a2fbc
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-10-19 09:01:45 +0700

    them scriptable object cho kitchen object

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
         Debug.Log("interact");
-        Instantiate(tomatoPrefab, counterTopPoint);
-
+        Transform s = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        s.localPosition = new Vector3(0, 0, 0);
+        Debug.Log(s.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
